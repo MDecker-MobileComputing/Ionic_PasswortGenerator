5e2c0cbe1276cc6f917d84f250bb40d36e88abe7
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-06-16 22:00:50 +0200

    Package-ID geaendert, Android-Projekt neu erzeugt

--- android/app/src/main/java/de/mide/ionic/passwortgenerator/MainActivity.java
+++ android/app/src/main/java/de/mide/ionic/passwortgenerator/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package de.mide.ionic.passwortgenerator;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PasswortGenerator',
+  appId: 'de.mide.ionic.passwortgenerator',
+  appName: 'Passwort-Generator',
   webDir: 'www',
   bundledWebRuntime: false
 };
