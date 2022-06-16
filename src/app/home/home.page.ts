d5f4f33b3664c0d9692a5f54256031ba4533c1ff
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-06-16 12:32:54 +0200

    HTML-Seite vorerst fertig

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public zeichenvorrat : string = "kleinundgrossbuchstaben";
+
+  public sicherheitslevel : string = "mittel";
+
   constructor() {}
 
 }
